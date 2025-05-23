117cdc3fe15ccb1bba61afee495cb4a792870bca
mhishmeh <hishmoe@gmail.com>
2025-05-23 17:02:49 -0500

    CCP - 3 - added footer POM and Base page footer spec file

---
+++ cypress/e2e/integration/homeFooter.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+import HomePageFooter from "../../../Pages/HomePageFooter"
+
+describe('TG100 G7 - Validate Base Page Footr', () => {
+    beforeEach(() => {
+        cy.visit('https://www.techglobal-training.com/')
+        
+    })
+    const homePageFooter = new HomePageFooter()
+
+    it('Validate Footer contains proper elements', () => {
+        homePageFooter.getFooterLogo()
+        homePageFooter.getAllFooterText()
+        homePageFooter.getFooterTextingText().should('be.visible')
+        homePageFooter.getFooterBackEndTestingText()
+        homePageFooter.getFooterExercisesText()
+        homePageFooter.getFooterJavaExercisesText()
+        homePageFooter.getFooterJsExerciesText()
+        homePageFooter.getFooterTextingText()
+        homePageFooter.getMockInterviewsButton()
+    })
+})
